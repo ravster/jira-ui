434a241c30c08c487988b1f4f417faf2a6ba7357
Ravi Desai <ravi@ravidesai.com>
2020-06-05 20:51:41 -0400

    Make comments using Python3

--- jira.py
+++ jira.py
@@ -17,6 +17,8 @@ def goto_issue(id):
     r = requests.get(uri, auth=(basic_auth[0], basic_auth[1]))
     global current_issue
     current_issue = r.json()
+    global issue_id
+    issue_id = id
 
 def description():
     print(current_issue['fields']['description'])
@@ -40,7 +42,22 @@ def my_issues():
     r = requests.get(uri, auth=(basic_auth[0], basic_auth[1]), params=body)
     r1 = r.json()
     for issue in r1['issues']:
-        print(f"{issue['key']}: {issue['fields']['summary']}")
+        print(f"{issue['key']}: {issue['fields']['status']['name']}: {issue['fields']['summary']}")
+
+def make_comment():
+    print("Write your comment and end with a '.' on it's own line.")
+    a1 = []
+    while True:
+        a2 = input('|')
+        if (len(a2) == 1 and a2[0] == '.'):
+            break
+        a1.append(a2)
+    a3 = "\n".join(a1)
+
+    uri = f'https://{subdomain}.atlassian.net/rest/api/2/issue/{issue_id}/comment'
+    r = requests.post(uri, auth=(basic_auth[0], basic_auth[1]), json={'body': a3})
+    r1 = r.json()
+    print(r1)
 
 def read_command():
     c = input("> ")
@@ -56,6 +73,8 @@ def read_command():
         print_all()
     elif c.startswith('mine'):
         my_issues()
+    elif c.startswith('m'):
+        make_comment()
     else:
         did_not_understand_command()
 
