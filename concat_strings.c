70a7a7284a348964b2ffb5afbacc2d8a56d92559
Ravi Desai <ravi@ravidesai.com>
2021-12-11 17:00:15 -0500

    Replicate String-join in C

--- concat_strings.c
+++ concat_strings.c
@@ -9,18 +9,37 @@ char* merge(int count, char** arr) {
   strcat(new, "");
   for(int i = 0; i < count; i++) {
     char* el = arr[i];
-    printf("i=%d, new=%s, arr[i]=%s\n---\n",
-	   i, new, el);
     strcat(new, el);
   }
   return new;
+}
+
+char* str_join(int count, char** input, char* sep) {
+  int sep_length = strlen(sep);
+  int tot_length = 0;
+  for(int i = 0; i < count; i++) {
+    tot_length += strlen(input[i]) + sep_length;
+  }
+  char* out = calloc(tot_length, sizeof(char));
+  strcat(out, "");
+  for(int i = 0; i<count; i++) {
+    char* el = input[i];
+    strcat(out, el);
+    if (i < (count-1)) {
+      strcat(out, sep);
+    }
+  }
+  return(out);
 }
 
 int main() {
   int count = 5;
   char* arr[] = {"a", "b", "c", "d", "e"};
   char* c = merge(count, arr);
+  char* d = str_join(count, arr, " and ");
   printf("c=%s\n", c);
+  printf("d=%s\n", d);
   free(c);
+  free(d);
   return 0;
 }
