f1b6bab141e973dba806d9733713a19e5369f3bb
Ravi Desai <ravi@ravidesai.com>
2021-12-11 16:43:49 -0500

    Simplify even further

--- concat_strings.c
+++ concat_strings.c
@@ -21,27 +21,6 @@ int main() {
   char* arr[] = {"a", "b", "c", "d", "e"};
   char* c = merge(count, arr);
   printf("c=%s\n", c);
-  for(int i = 0; i<count; i++) {
-    free(arr[i]);
-  }
-  //free(arr);
-  free(c);
-}
-
-int v1() {
-  int count = 5;
-  char** arr = calloc(count, sizeof(char*));
-  arr[0] = strdup("foo");
-  arr[1] = strdup("bar");
-  arr[2] = strdup("baz");
-  arr[3] = strdup("qux");
-  arr[4] = strdup("quux");
-  char* c = merge(count, arr);
-  printf("c=%s\n", c);
-  for(int i = 0; i<count; i++) {
-    free(arr[i]);
-  }
-  free(arr);
   free(c);
   return 0;
 }
